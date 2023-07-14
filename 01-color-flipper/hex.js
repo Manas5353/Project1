3fbb23b7445e4adfd33f9622b335f45ddd50f979
Manas Sharma <137026362+Manas5353@users.noreply.github.com>
2023-07-14 10:10:07 +0530

    Update hex.js

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,4 +1,15 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn"); //Button
 const colour = document.querySelector(".color"); //color span
-btn.addEventListener("click", function () {});
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomColor()];
+  }
+  colour.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+
+  function getRandomColor() {
+    return Math.floor(Math.random() * hex.length);
+  }
+});
